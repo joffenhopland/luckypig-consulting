472cf0ad0d5a05ef152b2832bed244eda3610a61
Cecile <cla040@uit.no>
2023-03-02 12:05:50 +0100

    loggin+view user info + oppdatere info + oppdatere password

---
+++ UserLoginForm.py
@@ -0,0 +1,39 @@
+from wtforms import StringField, SubmitField, EmailField, PasswordField
+from wtforms.validators import DataRequired, Email
+from flask_wtf import FlaskForm
+
+"""
+class UpdatePasswordForm(FlaskForm):
+    oldpassword = PasswordField("Old password", validators=[DataRequired()])
+    password1 = PasswordField("New password", validators=[DataRequired()])
+    password2 = PasswordField("Repeat new password", validators=[DataRequired()])
+
+    update = SubmitField("Change password")
+"""
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired(),Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+    login = SubmitField("Log in")
+
+class forgetPasswordForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    password1 = PasswordField("New password", validators=[DataRequired()])
+    password2 = PasswordField("Confirm new password", validators=[DataRequired()])
+
+    sumit = SubmitField("Sumit")
+
+class UpdateUserForm(FlaskForm):
+    firstname = StringField("First name", validators=[DataRequired()])
+    lastname = StringField("Last name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+
+    update = SubmitField("Update info")
+
+class UpdatePasswordForm(FlaskForm):
+    oldpassword = PasswordField("Old password", validators=[DataRequired()])
+    password1 = PasswordField("New password", validators=[DataRequired()])
+    password2 = PasswordField("Repeat new password", validators=[DataRequired()])
+
+    update = SubmitField("Change password")
