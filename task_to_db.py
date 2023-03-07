bdce287a444c5ec3e524168ae61aa34642e7614a
kristoffer <kpe144@uit.no>
2023-03-07 23:15:09 +0800

    insert into database

--- task_to_db.py
+++ task_to_db.py
@@ -17,10 +17,96 @@ class db:
         return self
     
     def read_file(self):
-        import pandas as pd
-        data = pd.read_csv("questions.csv")
-        data1 = data.iloc[0]
-        print(data1)
+        data = pd.read_csv("new_questions.csv", delimiter=";", encoding= 'unicode_escape')
+        for i in range(len(data)):
+            line = data.iloc[i].tolist()
+            question = line[1:6]
+            choice = line[6:]
+            if line[0] == 1:
+                self.insert_drop(question, choice)
+            elif line[0] == 3:
+               self.insert_multiple(question, choice)
+            elif line[0] == 5:
+                self.insert_drag(question, choice)
+    
+    def insert_drop(self, question, choice):
+        try:
+            conn = mysql.connector.connect(**self.configuration)
+            cursor = conn.cursor()
+            sql1 = '''INSERT INTO drop_down (level, themeId, question, answere, score)
+                VALUES (%s, %s, %s, %s, %s)'''
+            cursor.execute(sql1, question)
+            conn.commit()
+            id = cursor.execute("SELECT IDENT_CURRENT('drop_down')")
+            conn.close()
+        except mysql.connector.Error as err:
+            print(err)
+
+        for choices in choice:
+            val = (choices, id)
+            try:
+                conn = mysql.connector.connect(**self.configuration)
+                cursor = conn.cursor()
+                sql1 = '''INSERT INTO drop_down_choice (choice, dropId)
+                    VALUES (%s, %s)'''
+                cursor.execute(sql1, val)
+                conn.commit()
+                conn.close()
+            except mysql.connector.Error as err:
+                print(err)
+
+    
+    def insert_multiple(self, question, choice):
+        try:
+            conn = mysql.connector.connect(**self.configuration)
+            cursor = conn.cursor()
+            sql1 = '''INSERT INTO multiple_choice (level, themeId, question, answere, score)
+                VALUES (%s, %s, %s, %s, %s)'''
+            cursor.execute(sql1, question)
+            conn.commit()
+            id = cursor.execute("SELECT IDENT_CURRENT('multiple_choice')")
+            conn.close()
+        except mysql.connector.Error as err:
+            print(err)
+
+        for choices in choice:
+            val = (choices, id)
+            try:
+                conn = mysql.connector.connect(**self.configuration)
+                cursor = conn.cursor()
+                sql1 = '''INSERT INTO multiple_choice_choice (choice, multipleId)
+                    VALUES (%s, %s)'''
+                cursor.execute(sql1, val)
+                conn.commit()
+                conn.close()
+            except mysql.connector.Error as err:
+                print(err)
+    
+    def insert_drag(self, question, choice):
+        try:
+            conn = mysql.connector.connect(**self.configuration)
+            cursor = conn.cursor()
+            sql1 = '''INSERT INTO drag_and_drop (level, themeId, question, answere, score)
+                VALUES (%s, %s, %s, %s, %s)'''
+            cursor.execute(sql1, question)
+            conn.commit()
+            id = cursor.execute("SELECT IDENT_CURRENT('drag_and_drop')")
+            conn.close()
+        except mysql.connector.Error as err:
+            print(err)
+
+        for choices in choice:
+            val = (choices, id)
+            try:
+                conn = mysql.connector.connect(**self.configuration)
+                cursor = conn.cursor()
+                sql1 = '''INSERT INTO drag_choice (choice, dragId)
+                    VALUES (%s, %s)'''
+                cursor.execute(sql1, val)
+                conn.commit()
+                conn.close()
+            except mysql.connector.Error as err:
+                print(err)
     
 
 def main():
