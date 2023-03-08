2aa8f76147824d1772919058264c76a12bf45f4a
Cuichan <cch046@post.uit.no>
2023-03-08 19:26:41 +0100

    fjerne ubrukelig hmtl og py.fil

--- test.py
+++
@@ -1,6 +0,0 @@
-dragdrop = [{"id": 3, "text": "apple"}, {"id": 1, "text": "I"}, {"id": 2, "text": "like"}]
-
-
-correct_answer = [x["id"] for x in dragdrop]
-correct_answer.sort()
-print(correct_answer)
