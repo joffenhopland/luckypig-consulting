03397fa8dcb91a4025e5f7a7669517c63b1cb997
Cecile <cla040@uit.no>
2023-03-08 09:28:19 +0100

    henter oppgavene fra database og oppdaterer oppgaven med number_asked og number_succeed database koden kan brukes for dropdown og draganddrop oppgaver missing: inkrement til neste oppgave fra course

--- classes.py
+++ classes.py
@@ -1,25 +1,23 @@
 from database import db
 
 class MultipleChoiceExercise:
-    def __init__(self, exerciseID,level, question, answer, score, number_asked, number_succeed, themeId, choices):
+    def __init__(self, exerciseID):
         self.exerciseID = exerciseID
-        self.level = level
-        self.question = question
-        self.answer = answer
-        self.score = score
-        self.number_asked = number_asked
-        self.number_succeed = number_succeed
-        self.themeId = themeId
-        self.choices = choices
+        self.level = 0
+        self.score = 0
+        self.number_asked = 0
+        self.number_succeed = 0
+        self.themeId = 0
         self.type = 3
+        self.question = ''
+        self.answer = ''
+        self.choices = []
 
-    def getExerciseByID(self, exerciseID):
+    def getExercise(self):
         database = db()
-        exerciseID = 1
 
         #fill exercise info
-        (exerciseID, level, question, answer, score, number_asked, number_succeed, themeId) = database.getExerciseById(exerciseID, self.type)
-        self.exerciseID = exerciseID
+        (exerciseID, level, question, answer, score, number_asked, number_succeed, themeId) = database.getExerciseByIdandType(self.exerciseID, self.type)
         self.level = level
         self.question = question
         self.answer = answer
@@ -29,10 +27,10 @@ class MultipleChoiceExercise:
         self.themeId = themeId
 
         #fill option
-        choices = database.getOptionsByExerciseId(exerciseID, self.type)
+        choices = database.getOptionsByExerciseIdandType(self.exerciseID, self.type)
         self.choices = choices
 
     def updateExercise(self):
         database = db()
-        database.updateExerciseByExerciseIdandType(self.exerciseID,self.type,self.number_asked,self.number_succeed)
+        database.updateExerciseByExerciseIdandType(self.exerciseID,self.type,str(self.number_asked),str(self.number_succeed))
 
